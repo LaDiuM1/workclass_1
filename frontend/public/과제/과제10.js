2fd8bddc615975f4df9641af86e26c025e1e0197
LaDiuM <LaDiuM@192.168.219.108>
2023-06-21 23:21:06 +0900

    230621 23:21

--- frontend/public/과제/과제10.js
+++ frontend/public/과제/과제10.js
@@ -1,32 +1,45 @@
-/**
- * 
- */
 
  let id_arr = [ ]
  let password_arr = [ ]
  
  function sign_up(){ //f start
 	 
-	 // 로그인 정보 받기
+
 	 let id_input = document.querySelector(".login_id")
 	 let id_value = id_input.value
-	 
-	 if(id_arr.indexOf(id_value)!= -1){alert('이미 가입된 회원입니다.')}
-	 else{
-	 id_arr.push(id_value)
-	 id_input.value = ''
-	 
-	 
-	 // 비밀번호 정보 받기
+
 	 let password_input = document.querySelector(".login_password")
 	 let password_value = password_input.value
+	 
+	if (id_value =='') {alert('Enter your user id and password.')} 
+		else if(id_arr.indexOf(id_value)!= -1){alert('This account is already in use. Please try again.')}
+		else if(id_value.length<8 || password_value.length<8) {alert('id and password must have at least 8 characters. try again.')}
+	 	else{
+	
+	 id_arr.push(id_value)
 	 password_arr.push(password_value)
 	 
-	 loginUl = document.querySelector(".login_list")
-	 loginUl.innerHTML = '<li>' +'id : '+id_arr+'<br/>'+'password : '+password_arr+'</li>'
-	 
+	 id_input.value = ''
 	 password_input.value = ''
-	 alert('회원가입 완료.')
+	 alert('register successfully')
+	
 	 }
+	 
+ }//f end
+
+function sign_in(){ //f start
+	 
+
+	 let id_input = document.querySelector(".login_id2")
+	 let id_value = id_input.value
+
+	 let password_input = document.querySelector(".login_password2")
+	 let password_value = password_input.value
+	 
+	 if (id_value =='') {alert('Enter your user id and password.')}
+		 else if(id_arr.indexOf(id_value)!= -1 && password_arr.indexOf(password_value)!= -1){alert('Login successfully.')
+			 id_input.value = ''
+			 password_input.value = ''}
+		 else {alert('Login failed.')}
 	 
  }//f end
