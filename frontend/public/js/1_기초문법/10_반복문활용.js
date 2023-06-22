4fa76bda41023b75f9eb4fbd3fc652e1474001fa
LaDiuM <LaDiuM@192.168.219.108>
2023-06-23 00:28:02 +0900

    23/06/23 00:27

--- frontend/public/js/1_기초문법/10_반복문활용.js
+++ frontend/public/js/1_기초문법/10_반복문활용.js
@@ -1,157 +1,179 @@
 /*
-// 문제1) 입력받은 수 만큼 * 출력 
-// 문제2) 입력받은 수 만큼 * 출력 [  - 3줄(3배수)마다 줄바꿈 ]
-// 문제3) 입력받은 줄 수 만큼 * 출력  [ ex) 5 ]
-*
-**			
-***		
-****	
-*****
-// 문제4) 입력받은 줄 수 만큼 * 출력 [ ex) 5 ]
-*****
-****
-***
-**
-*
-// 문제5) 입력받은 줄 수 만큼 * 출력  [ ex) 5 ]
-    *
-   **
-  ***
- ****
-*****
-// 문제6) 입력받은 줄 수 만큼 * 출력  [ ex) 5 ]
-*****
- ****
-  ***
-   **
-    *
-// 문제7) 입력받은 줄 수 만큼 * 출력  [ ex) 5 ]
-    *
-   ***
-  *****
- *******
-*********
-// 문제8) 입력받은 줄 수 만큼 * 출력  [ ex) 5 ]
-*********
- *******
-  *****
-   ***
-    *
-// 문제9) 입력받은 줄 수 만큼 * 출력  [ ex) 10 ]
-    *
-   ***
-  *****
- *******
-*********
-*********
- *******
-  *****
-   ***
-    *
-// 문제10) 입력받은 줄 수 만큼 * 출력  [ ex) 7 ]
-*     *
- *   *
-  * *
-   *
-  * *
- *   *
-*     *
+console.log('-----문제1-----')
+let int = Number(prompt('문제1 : 별의 개수를 입력하세요.'));
 
-*/
+let output = ``
 
-let output = ``;	//  출력용 변수 [ 모든 문제 에서 사용 ]
-// 문제1) 입력받은 수 만큼 * 출력 
-let value1 = prompt( '문제1:')
-for( let i = 1 ; i<=value1 ; i++  ){ // i는 1부터 입력받은 수까지 1씩증가반복 
+for(let i=1; i<=int; i++ ){
 	output += `*`
-} // for end 
-console.log( output )
-// ---------------------------------------------------------- // 
-// 문제2) 입력받은 수 만큼 * 출력 [  - 3줄(3배수)마다 줄바꿈 ]
-output = ``; // 공백으로 초기화
-let value2 = prompt( '문제2:')
-for( let i = 1 ; i<=value2 ; i++ ){
+}
+	console.log(output)
+	
+console.log('-----문제2-----')
+int = Number(prompt('문제2 : 별의 개수를 입력하세요.'));
+
+output = ``
+
+for(let i=1; i<=int; i++ ){
 	output += `*`
-	// 만약에 i의 값이 3배수( 값%3==0 ) 이면 
-	if( i % 3 == 0 ){ output += `\n` }	
-	// \ : 제어문자(이스케이프문자)	\t : 들여쓰기 \n : 줄바꿈
-} // for end 
-console.log( output )
-
-// ---------------------------------------------------------- //
-// 문제3) 입력받은 줄 수 만큼 * 출력  [ ex) 5 ]
-output = ``;
-let line1 = prompt( '문제3:')
-for( let i = 1 ; i<=line1 ; i++ ){
-	// [별찍기]
-	for( let s = 1 ; s <= i ; s++){ output += `*`}
-	// [줄바꿈] i는 1부터 입력받은 줄수까지 1씩증가
-	output +=`\n`;
-}
-console.log( output )
-// ---------------------------------------------------------- //
-// 문제4) 입력받은 줄 수 만큼 * 출력  [ ex) 5 ]
-output = ``;
-let line2 = prompt( '문제4:')
-for( let i = 1 ; i<=line2 ; i++ ){
-	// [별찍기]
-	for( let s = 1 ; s<=line2-i+1 ; s++ ){ output += `*`}
-	// [줄바꿈]
-	output += `\n`
-} // for end 
-console.log( output )
-// 문제5) 입력받은 줄 수 만큼 * 출력  [ ex) 5 ]
-output = ``;
-let line3 = prompt( '문제5:')
-for( let i = 1 ; i<=line3 ; i++ ){
-	// 2. [공백 출력]
-	for( let b = 1 ; b<=line3-i ; b++ ){ output += ` ` } // 스페이스바 한번 = 공백1개 
-	// 3. [별 출력 ]
-	for( let s = 1 ; s<=i ; s++ ){ output += `*`}
-	// 1. [줄바꿈]
-	output += `\n` // i마다 줄바꿈 => 줄마다 줄바꿈
-}
-console.log( output )
-// 문제6) 입력받은 줄 수 만큼 * 출력  [ ex) 5 ]
-output = ``;
-let line4 = prompt( '문제6:')
-for( let i = 1 ; i<=line4 ; i++ ){
-	// [공백 출력]
-	for( let b = 1 ; b<= i-1 ; b++ ){ output += ` `}
-	// [별 출력 ]
-	for( let s = 1 ; s<= line4-i+1 ; s++ ){ output += `*` }
-	// [줄바꿈 출력]
-	output += `\n`
-}
-console.log( output )
-
-console.log('----문제7----')
-
-let int = Number(prompt('정수를 입력하세요.(문제7)'))
-
-output=`*`
-
-let space = 0;
-for(let i=1; i<=int; i++){
-	if(i%2==1){ space += 1 }
+	if(i%3==0){output += `\n`}
 	
 }
-console.log(space)
+	console.log(output)
+	
+console.log('-----문제3-----')
+int = Number(prompt('문제3 : 별의 개수를 입력하세요.'));
+
+output = ``
+
+for(let i=1; i<=int; i++ ){
+	
+	console.log(output += `*`)
+}
+	
+	
+console.log('-----문제4-----')
+int = Number(prompt('문제4 : 별의 개수를 입력하세요.'));
+
+output = ``
+
+for(let i=1; i<=int; i++){
+	for(let j=int-i+1; j>0; j--){
+			output += `*`
+			}
+	output += `\n`
+	console.log(output)
+	output = ``
+}
+
+console.log('-----문제5-----')
+int = Number(prompt('문제5 : 별의 개수를 입력하세요.'));
+
+output = ``
+
+for(let i=1; i<=int; i++){
+	for(let j=int-i; j>0; j--){
+		output += ` `
+	}
+	for(let k=1; k<=i; k++){
+		output +=`*`
+	}	
+output += `\n`
+console.log(output)
+output = ``
+	
+}
+
+console.log('-----문제6-----')
+int = Number(prompt('문제6 : 별의 개수를 입력하세요.'));
+
+output = ``
+
+for(let i=1; i<=int; i++){
+	for(let k=0; k<i-1; k++){
+	output +=` `
+	}
+	for(let j=int-i+1; j>0; j--){
+		output +=`*`
+	}	
+
+output += `\n`
+console.log(output)
+output = ``
+
+}
+
+console.log('-----문제7-----')
+int = Number(prompt('문제7 : 별의 개수를 입력하세요.'));
+
+output = ``
+
+for(let i=1; i<=int; i++){
+	for(let j=int-i; j>0; j--){
+		output +=` `
+	}
+	for(let k=0; k<i; k++){
+		if(k==0){output +=`*`}
+		else{
+		output +=`**`}
+	}
+ output += `\n`
+console.log(output)
+output = ``
+	
+}
+
+console.log('-----문제8-----')
+int = Number(prompt('문제8 : 별의 개수를 입력하세요.'));
+
+output = ``
 
 for(i=1; i<=int; i++){
-	
-	if(i%2==1){
-		for()
+	for(j=2; j<=i; j++){
+	output +=` `
+	}
+	output +=`*`
+	for(k=int-i; k>0; k--){
+	output +=`**`
+	}
+output += `\n`
+console.log(output)
+
+output = ``
+}
+
+console.log('-----문제9-----')
+let int1 = Number(prompt('문제9 : 별의 개수를 입력하세요.'));
+
+int = int1/2
+output = ``
+
+for(let i=1; i<=int; i++){
+	for(let j=int-i; j>0; j--){
+		output +=` `
+	}
+	for(let k=0; k<i; k++){
+		if(k==0){output +=`*`}
+		else{
+		output +=`**`}
+	}
+ output += `\n`
+console.log(output)
+output = ``
+}
+
+for(i=1; i<=int; i++){
+	for(j=2; j<=i; j++){
+	output +=` `
+	}
+	output +=`*`
+	for(k=int-i; k>0; k--){
+	output +=`**`
+	}
+output += `\n`
+console.log(output)
+
+output = ``
+}
+
+console.log('-----문제10-----')
+int = Number(prompt('문제10 : 별의 개수를 입력하세요.'));
+
+output = ``
+
+for(let i=1; i<=int+2; i++){
+	for(let j=0; j<=i; j++){
+		if(j)
 		
-		
-		
-		
-	 }
-	
-	
-	
+	}
+
+
+output += `\n`
 }
 
 
 
 
+
+
+	
