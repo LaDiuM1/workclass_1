4d63bbb02feee90d6a26571c15e0614704ad8dbf
LaDiuM <LaDiuM@192.168.219.108>
2023-06-26 23:33:19 +0900

    23/06/26 과제3 완료

--- frontend/public/과제/3_과제/과제3.js
+++ frontend/public/과제/3_과제/과제3.js
@@ -1,20 +1,24 @@
-let sum = 0;
+let sum = 0;  let date = []; let item = []; let price = [];
 
-let date = [];
-let item = [];
-let price = [];
 
 function submit(){
+	
 	let dateData = document.querySelector('.dateInput')
 	dateValue = dateData.value
-	date.push(dateValue)
 	
 	let itemData = document.querySelector('.itemInput')
 	itemValue = itemData.value
-	item.push(itemValue)
 	
 	let priceData = document.querySelector('.priceInput')
 	priceValue = priceData.value
+	
+	if(dateData.value == `` || itemData.value == `` || priceData.value == `` ){
+		alert('날짜,항목,금액을 모두 기입해 주세요.');
+		return;
+	}
+
+	date.push(dateValue)
+	item.push(itemValue)
 	price.push(priceValue)
 	
 	listAdd()
@@ -33,10 +37,10 @@ function listAdd(){
 								<td>${date[i]}</td>
 								<td>${item[i]}</td>
 								<td>${Number(price[i]).toLocaleString()}원</td>
-								<td><button onclick="delPcn(${i})" type="button">삭제</button></td>
+								<td><button onclick="delPcn(${i})" type="button" style="width : 50px;">삭제</button></td>
 								 </tr>`
 	}
-	if(date.length!=0){tableHTML +=`<td colspan='2'>총합계 : </td><td >${sumPcn()}원</td>`}
+	if(date.length!=0){tableHTML +=`<td colspan='4'>총합계 : ${sumPcn()}원</td>`}
 				
 	tableDiv.innerHTML = tableHTML
 	
