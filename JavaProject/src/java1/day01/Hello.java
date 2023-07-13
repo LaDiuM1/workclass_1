54452d956610ed812a1d4c89f5c1ced072d4bc8c
504 <504@DESKTOP-3IJG99V>
2023-07-13 16:15:09 +0900

    23/07/13 16:14 정용상 java 최초 commit

---
+++ JavaProject/src/java1/day01/Hello.java
@@ -0,0 +1,23 @@
+// 한줄 주석
+/* 여러줄 주석 */
+
+
+package java1.day01;
+
+public class Hello { // 클래스 선언 //  'Hello' 클래스명 (변수 선언과 규칙 동일, 첫글자는 대문자여야함)
+	// { } 마다 tab 1번 적용
+	public static void main(String[] args) { // 메서드 시작
+		// 1. 모든 코드는 클래스 안에서 작성
+		// 2. 모든 실행코드는 메인 함수 안에서 작성
+		
+		
+		// console.log() vs System.out.println()
+		// syso + 컨트롤 + 스페이스바
+		System.out.println("Hello Java");
+		//ctrl+F11 실행
+	
+	}//메서드 끝
+	
+	//실행코드는 메인 메서드 밖에서 사용 불가
+	
+}// 클래스 끝
